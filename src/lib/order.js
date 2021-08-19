c18c435c17dca26c3f7bc9b4f6ad0bc806ade448
Imac <imac@iMac-de-Imac.local>
2021-08-19 12:00:58 -0500

    [UPDATE] order model

--- src/lib/order.js
+++ src/lib/order.js
@@ -5,23 +5,61 @@ function setupOrder ({UserModel, OrderModel, ProductModel, CompanyModel}) {
             user = user._id;
         }
 
-        user = UserModel.findOne({ _id: user });
+        user = await UserModel.findOne({ _id: user });
 
-        if (!user) throw new Error('The user doesnt exit');
-        if (!user.active) throw new Error('The user isnt active');
+        if (!user) throw new Error('El usuario no existe en el sistema');
+        if (!user.active) throw new Error('El usuario no esta activo');
 
-        if (!productList.length) throw new Error('You cant create an order without any product');
+        if (!productList.length) throw new Error('Los pedidos deben tener por lo menos un producto');
         
-        await OrderModel.create({
+
+        const order = await OrderModel.create({
             user: user._id,
+            products: productList
         });
 
-        return {}
+        return order;
     }
 
     
-    function getAll () {
-        return {}
+    async function getAll(user) {
+        const query = {};
+
+        if(user) {
+            if (typeof user === 'object') {
+                user = user._id;
+            }
+            query.user = user
+        }
+
+        // obtengo todas las ordenes
+        let orders = await OrderModel.find(query).sort({ createdAt: -1 }).exec();
+
+        //reviso que a que usuarios pertenecen esas ordenes
+        let users = orders.reduce((usersId, order) => {
+            if (usersId.indexOf(order.user) !== -1) {
+                return usersId;
+            }
+            return [...usersId, order.user]
+        }, []);
+        // traigo los usuarios de la base de datos
+        users = await UserModel.find({ _id: { $in: users } });
+
+        // Me traigo todos los productos
+        const products = await ProductModel.find({ active: true });
+
+        // Modifico las ordenes para incluir todos los valores
+        orders = orders.map((order) => {
+            order.id = order._id;
+            order.user = users.find(u => u._id === order.user);
+            order.products = order.products.map(product => ({
+                ...products.find((p) => p._id === product._id),
+                details: product.details
+            }));
+            return order;
+        })
+
+        return orders;
     }
 
     
