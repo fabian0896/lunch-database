42c1e7d462849f922ba0ea7dd64ee666721a882f
Imac <imac@iMac-de-Imac.local>
2021-08-19 16:21:52 -0500

    [ADD] pagination to the orders

--- src/lib/order.js
+++ src/lib/order.js
@@ -1,7 +1,7 @@
-function setupOrder ({UserModel, OrderModel, ProductModel, SerialModel}) {
-   
-    async function create (user ,productList) {
-        if (typeof user === 'object'){
+function setupOrder({ UserModel, OrderModel, ProductModel, SerialModel, CompanyModel }) {
+
+    async function create(user, productList) {
+        if (typeof user === 'object') {
             user = user._id;
         }
 
@@ -11,7 +11,7 @@ function setupOrder ({UserModel, OrderModel, ProductModel, SerialModel}) {
         if (!user.active) throw new Error('El usuario no esta activo');
 
         if (!productList.length) throw new Error('Los pedidos deben tener por lo menos un producto');
-        
+
         const consecutive = await SerialModel.getConsecutive('orders');
 
 
@@ -24,20 +24,8 @@ function setupOrder ({UserModel, OrderModel, ProductModel, SerialModel}) {
         return order;
     }
 
-    
-    async function getAll(user) {
-        const query = {};
 
-        if(user) {
-            if (typeof user === 'object') {
-                user = user._id;
-            }
-            query.user = user
-        }
-
-        // obtengo todas las ordenes
-        let orders = await OrderModel.find(query).sort({ createdAt: -1 }).exec();
-
+    async function addAllValues(orders) {
         //reviso que a que usuarios pertenecen esas ordenes
         let users = orders.reduce((usersId, order) => {
             if (usersId.indexOf(order.user) !== -1) {
@@ -48,13 +36,24 @@ function setupOrder ({UserModel, OrderModel, ProductModel, SerialModel}) {
         // traigo los usuarios de la base de datos
         users = await UserModel.find({ _id: { $in: users } });
 
+        //me traigo las compañias de los usuarios
+        let companies = users.map((u) => u.company);
+        companies = await CompanyModel.find({ _id: { $in: companies } })
+
         // Me traigo todos los productos
         const products = await ProductModel.find({ active: true });
 
         // Modifico las ordenes para incluir todos los valores
         orders = orders.map((order) => {
             order.id = order._id;
+
+            //Agrego el usuario
             order.user = users.find(u => u._id === order.user);
+
+            //Agrego la company
+            order.user.company = companies.find(c => c._id === order.user.company);
+
+            //Introduzco la información actual de los productos
             order.products = order.products.map(product => ({
                 ...products.find((p) => p._id === product._id),
                 uniqueId: product.cartId || product._id,
@@ -62,11 +61,42 @@ function setupOrder ({UserModel, OrderModel, ProductModel, SerialModel}) {
             }));
             return order;
         });
-
         return orders;
     }
 
-    
+
+    async function getAll(limit=5, user) {
+        const query = {};
+        let count = limit;
+
+        if (user) {
+            if (typeof user === 'object') {
+                user = user._id;
+            }
+            query.user = user
+        }
+
+        // obtengo todas las ordenes
+        let orders = await OrderModel.find(query).sort({ createdAt: -1 }).limit(limit).exec();
+
+        orders = await addAllValues(orders);
+
+        async function nextFunc () {
+            let orders = await OrderModel
+                                    .find(query)
+                                    .sort({ createdAt: -1 })
+                                    .skip(count)
+                                    .limit(limit)
+                                    .exec();
+            count += limit;
+            orders = await addAllValues(orders);
+            return {data: orders, next: nextFunc};
+        }
+
+        return { data: orders, next: nextFunc};
+    }
+
+
     return {
         create,
         getAll
